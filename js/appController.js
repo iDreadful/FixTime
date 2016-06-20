a7f23c8ba650953a7dd7ba309a9d157d6383f945
Alex Pestov <pestov@dopestov.denver.cqg>
2016-06-20 11:35:12 -0600

    Revert "Update appController.js"
    This reverts commit 02afb9239428d4b75d7b94e576b047d835858d51.

--- js/appController.js
+++ js/appController.js
@@ -388,8 +388,7 @@ app.controller( 'appController', function( $scope, $http, $timeout ) {
 
         // Get data from URL chain
         function getData ( link, callback ) {
-            //var key = "AIzaSyANFw1rVq_vnIzT4vVOwIw3fF1qHXV7Mjw";
-            var key = "AIzaSyDBgo56txclouOSrm65C942oyvH36MFbBs";
+            var key = "AIzaSyANFw1rVq_vnIzT4vVOwIw3fF1qHXV7Mjw";
 
             // This will be repeated fo revery piece
             function iterator ( link, glue ) {
